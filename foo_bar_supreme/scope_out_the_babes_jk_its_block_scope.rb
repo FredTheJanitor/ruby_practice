6bb108bbf74d00afc160692fb997d5a03988fa49
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-12-21 18:30:00 -0500

    chore: look at while scope compared to loop

--- foo_bar_supreme/scope_out_the_babes_jk_its_block_scope.rb
+++ foo_bar_supreme/scope_out_the_babes_jk_its_block_scope.rb
@@ -1,6 +1,14 @@
-x = 2
-loop do
-  x = 42
-  break
+# x = 2
+# loop do
+#   x = 42
+#   break
+# end
+# puts x
+# 
+x = 0
+while x < 5
+  y = x * X
+  x += 1
 end
-puts x
+
+puts "ok so y was declared within the while loop and y is #{y}"
