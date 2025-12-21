879af189d804186533fd2f1240300e4fc878100c
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-12-21 13:09:29 -0500

    chore: do the until ish my bish

--- foo_bar_supreme/while_were_at_it.rb
+++ foo_bar_supreme/while_were_at_it.rb
@@ -1,11 +1,20 @@
-puts "Enter a number less than |10,000|"
+puts "Enter a number less than |1,000|"
 x = gets.chomp.to_i.abs
-while x >= 0
-  puts x
-  x -= 1
-  if x.abs >= 10000
-    puts "Error: X too high... lol 420"
+# while x >= 0
+#   puts x
+#   x -= 1
+#   if x.abs >= 10000
+#     puts "Error: X too high... lol 420"
+#     break
+#   end
+# end
+# puts "ay bay bay"
+until x < 0
+  if x.abs >= 1000
+    puts "the fuck I just say?"
     break
   end
+  puts x
+  x -= 1
 end
-puts "ay bay bay"
+puts "done"
