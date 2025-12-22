16da61e00c68ba2642e85b19c7a152a8ac1a4805
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-12-22 18:30:00 -0500

    chore: werk werk werk werk werk werk

---
+++ foo_bar_supreme/the_gauntlet.rb
@@ -0,0 +1,21 @@
+# i = 0
+# loop do
+#   puts "i is #{i}"
+#   i += 1
+#   break if i == 10
+# end
+
+# used Until:
+# i = 0
+# until i == 10
+#   puts "i is #{i}"
+#   i += 1
+# end
+# 
+# 2--------------------------------------
+# Given:
+i = 0
+while i < 10 do
+ puts "i is #{i}"
+ i += 1
+end
