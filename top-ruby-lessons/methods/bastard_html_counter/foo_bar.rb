5eb58f880980b7a425451439966c97a9397f08d6
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-01-10 10:45:41 -0500

    feat: create website img tag counter

--- top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
+++ top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
@@ -1,7 +1,31 @@
 require "open-uri"
-url = "http://www.nytimes.com"
-pattern = "<img"   
+# url = "https://www.nytimes.com"
+# pattern = "<img"   
 
-page = open(url).read
-tags = page.scan(pattern)
-puts "The site #{url} has #{tags.length} img tags"
+# page = open(url).read
+# # page = open is a monkey patch that doesnt work anymore
+# 
+# tags = page.scan(pattern)
+# puts "The site #{url} has #{tags.length} img tags"
+# 
+#
+# require "open-uri"
+
+# url = "https://www.nytimes.com"
+# pattern = "<img"
+
+# page = URI.open(url).read # updated open to URI.open
+# tags = page.scan(pattern)
+
+# puts "The site #{url} has #{tags.length} img tags"
+
+#  now we can methodize it to do any link
+#  i made it so i dont have to type out the whole link, and i can just interact with cli
+def count_image_tags(url = ("https://www." + gets.chomp.to_s + ".com"))
+  pattern = "<img"
+  page = URI.open(url).read
+  tags = page.scan(pattern)
+  puts "The site #{url} has #{tags.length} img tags"
+end
+
+count_image_tags
