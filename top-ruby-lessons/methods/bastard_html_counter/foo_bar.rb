d3d03e477a7e210c33f6dbbd14002a2cac1c6435
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-01-10 12:38:47 -0500

    feat: create basic user interface for CLI

--- top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
+++ top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
@@ -41,6 +41,51 @@ def count_any_tags(url = ("https://www." + gets.chomp.to_s + ".com"), tag = ("<"
   tags = page.scan(tag)
   puts "The site #{url} has #{tags.length} #{tag} tags"
 end
-puts "Enter Website (without https://www. or the .com just the name)"
-puts "After that, enter the tag you would like to count (again just the tag name)"
-count_any_tags
+def count_manual
+  puts "-------------------------Manual Mode-------------------------"
+  puts "Enter Website (without https://www. or the .com just the name)"
+  puts "After that, enter the tag you would like to count"
+  puts "(again just the tag name)" 
+  count_any_tags
+end
+def count_multiple
+  puts "-- =-------------------Multiple Mode-------------------------"
+  puts "**NOTE**"
+  puts "-- the current tags are fixed as a, div, and img --"
+  puts "--"
+  puts "Enter Website (without https://www. or the .com just the name)"
+  url = ("https://www." + gets.chomp.to_s + ".com")
+  tags = ["a", "div", "img"]
+  tags.each do |tag|
+    count_any_tags(url, tag)
+  end
+end
+def end_sequence
+  puts "------------------------------------------------------------"
+  puts "Would you like to count again? (Y to restart, N to exit)"
+  input = gets.chomp.to_s
+  unless ((input == "Y") || (input == "N"))
+    puts "Invalid Input"
+    end_sequence
+  end
+  if input == "Y"
+    count_picker
+  end
+end
+def count_picker
+  puts "-----------------Welcome to the count picker!---------------"
+  puts " "
+  puts "To get started, type 'man' for manual, or 'mul' for multiple"
+  input = gets.chomp.to_s
+  unless ((input == 'man') || (input == 'mul'))
+    puts "Invalid Input, please type either man or mul"
+    count_picker
+  end
+  if input == "man"
+    count_manual
+  else
+    count_multiple
+  end
+end_sequence
+end
+count_picker
