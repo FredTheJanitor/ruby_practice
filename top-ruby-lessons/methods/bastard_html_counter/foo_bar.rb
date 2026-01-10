f039d0ace3ae68ff6e0b4d84c1e7d506f3b4cbfa
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-01-10 18:30:00 -0500

    feat: follow DRY and create 1 flexible method

--- top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
+++ top-ruby-lessons/methods/bastard_html_counter/foo_bar.rb
@@ -28,4 +28,19 @@ def count_image_tags(url = ("https://www." + gets.chomp.to_s + ".com"))
   puts "The site #{url} has #{tags.length} img tags"
 end
 
-count_image_tags
+def count_div_tags(url = ("https://www." + gets.chomp.to_s + ".com"))
+  pattern = "<div"
+  page = URI.open(url).read
+  tags = page.scan(pattern)
+  puts "The site #{url} has #{tags.length} div tags"
+end
+
+# one method to rule them all DRY
+def count_any_tags(url = ("https://www." + gets.chomp.to_s + ".com"), tag = ("<" + gets.chomp.to_s))
+  page = URI.open(url).read
+  tags = page.scan(tag)
+  puts "The site #{url} has #{tags.length} #{tag} tags"
+end
+puts "Enter Website (without https://www. or the .com just the name)"
+puts "After that, enter the tag you would like to count (again just the tag name)"
+count_any_tags
