b0fcd25bbb7f055b1935b0946beee4aee96356ae
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-01-10 18:30:00 -0500

    fix: syntax error

--- top-ruby-lessons/methods/bastard_html_counter/counter-2.rb
+++ top-ruby-lessons/methods/bastard_html_counter/counter-2.rb
@@ -1,11 +1,23 @@
 require "open-uri"
 
 def fetch_page(url)
-  return open(url.read)
+  return URI.open(url).read
 end
 
 def just_count_tags(page, tag)
   pattern = /<#{tag}\b/
   tags = page.scan(pattern)
   return tags.length
+end
+
+sites = [ "https://www.theodinproject.com", "https://www.Google.com", "https://www.yahoo.com"]
+tags = ["div","h1","h2","h3","img","p"]
+
+sites.each do |url|
+  puts "#{url} has:"
+  tags.each do |tag|
+    page = fetch_page(url)
+    tag_count = just_count_tags(page, tag)
+    puts "\t - #{tag_count} <#{tag}> tags"
+  end
 end
