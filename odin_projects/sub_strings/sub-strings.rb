7c46dd622d41e4a2b249e0d0b8c1e7379622f69e
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-03 18:30:00 -0500

    chore: create dictionary array of substrings

---
+++ odin_projects/sub_strings/sub-strings.rb
@@ -0,0 +1,11 @@
+# instructions:
+# Implement a method #substrings that takes a word as the first argument 
+# and then an array of valid substrings (your dictionary) as the second 
+# argument. 
+
+dictionary = %w(that the is are was this re un de on at as in to by for with or from pre dis ing ed ly tion )
+
+
+# It should return a hash listing each substring 
+# (case insensitive) that was found in the original string and how many 
+# times it was found.
