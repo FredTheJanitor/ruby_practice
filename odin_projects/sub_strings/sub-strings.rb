7ea9a5ffc758018c9bdd25d6101a1dabc1b27884
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-03 18:30:00 -0500

    chore: implement some pseudocode

--- odin_projects/sub_strings/sub-strings.rb
+++ odin_projects/sub_strings/sub-strings.rb
@@ -5,6 +5,15 @@
 
 dictionary = %w(that the is are was this re un de on at as in to by for with or from pre dis ing ed ly tion )
 
+def substrings(string = "", array = [])
+  # search the string for each substring and find ALL instances
+  # create a method on string like find or select
+  # use an argument based on the array
+  # argument from array: for each member of the array
+  # if the string contains that member increase the tally
+  # if the string contains multiple of that member increase the
+  # tally that number of times
+end
 
 # It should return a hash listing each substring 
 # (case insensitive) that was found in the original string and how many 
