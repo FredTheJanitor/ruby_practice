94264d26dce235c6daed5d05a063e19a8aeb4f59
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-04 18:30:00 -0500

    feat: regex scan method for 1 substring of string

--- odin_projects/sub_strings/sub-strings.rb
+++ odin_projects/sub_strings/sub-strings.rb
@@ -4,14 +4,27 @@
 # argument. 
 
 dictionary = %w(that the is are was this re un de on at as in to by for with or from pre dis ing ed ly tion )
+# so based on this list of characters, I need to check the string for each one of these character combos 
+# and get all of them counted
+# lets try select
 
+
+
+dictio
 def substrings(string = "", array = [])
   substring_count = array.reduce({}) do |tally, substring|
     number_of_substrings = string.scan(substring)
+    p tally
     tally[substring] = number_of_substrings
   end
   substring_count
 end
+
+#ok so tally is more like ["a" "b" "c" ] => { "a" => 1, "b" => 2 etc}
+#
+
+string = gets.chomp
+substrings(string, dictionary)
 
 # It should return a hash listing each substring 
 # (case insensitive) that was found in the original string and how many 
