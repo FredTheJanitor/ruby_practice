2e1a1e5fa110444f9ee877d12805cb4b33b0212c
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-05 18:30:00 -0500

    chore: clean up comments and reference lesson link

--- odin_projects/sub_strings/sub-strings.rb
+++ odin_projects/sub_strings/sub-strings.rb
@@ -1,7 +1,16 @@
+# -----------------------------------------------------------------------------------------------------------
+# Lesson from: https://www.theodinproject.com/lessons/ruby-sub-strings
+#  
 # instructions:
 # Implement a method #substrings that takes a word as the first argument 
 # and then an array of valid substrings (your dictionary) as the second 
-# argument. 
+# argument.
+
+# It should return a hash listing each substring 
+# (case insensitive) that was found in the original string and how many 
+# times it was found.
+# 
+# -----------------------------------------------------------------------------------------------------------
 
 # default list of substrings to check against:
 dictionary = %w(that the is are was this re un de on at as in to by for with or from pre dis ing ed ly tion )
@@ -46,7 +55,3 @@ def substrings(string = "", dictionary = [])
 end
 
 
-
-# It should return a hash listing each substring 
-# (case insensitive) that was found in the original string and how many 
-# times it was found.
