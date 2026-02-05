b1b28f31332309e787a423cb2b4900e8e40c57a7
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-05 18:30:00 -0500

    chore: combine methods and remove case sensitivity

--- odin_projects/sub_strings/sub-strings.rb
+++ odin_projects/sub_strings/sub-strings.rb
@@ -3,10 +3,8 @@
 # and then an array of valid substrings (your dictionary) as the second 
 # argument. 
 
+# default list of substrings to check against:
 dictionary = %w(that the is are was this re un de on at as in to by for with or from pre dis ing ed ly tion )
-# so based on this list of characters, I need to check the string for each one of these character combos 
-# and get all of them counted
-# lets try select
 
 # uses regex to find all instances of a substring within another string
 # substring = dictionary.each
@@ -22,21 +20,29 @@ def count_all_substrings(string, dictionary)
     substring_counter(string, substring)
   end
   array_of_counts
+  # output is like [{}, {}, {"re" => 3}, {}, {"ly" => 2}]
 end
 
-# this should output an array of hashes: need to flatten it to 1d
-# this calls for reduce:
-
+# use reduce and merge to combine hashes
 def arr_of_hashes_to_hash(array_of_hashes)
 # found this method on stack overflow: 
 # https://stackoverflow.com/questions/11856407/rails-mapping-array-of-hashes-onto-single-hash
-# looks pretty clean lol - need to look into how merge works more.
   new_hash = array_of_hashes.reduce(:merge)
+  # the above is a shorthand for: 
+  # arr.reduce do |acc, el|
+  #   acc.merge(el)
   new_hash
 end
 
+# make a method that combines the 3 actions above into a cohesive unit
 def substrings(string = "", dictionary = [])
-  
+  # remove case sensitivity from string and dictionary
+  string = string.downcase
+  dictionary = dictionary.map { |substring| substring.downcase }
+  # from a dictionary of substrings, count how many times each occurs in a string, and output a hash with that count
+  array_of_hashes = count_all_substrings(string, dictionary)
+  result_hash = arr_of_hashes_to_hash(array_of_hashes)
+  result_hash
 end
 
 
