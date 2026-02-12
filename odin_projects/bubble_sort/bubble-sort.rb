57c78210132a3703f5cafb6a7cac7c8bb5901fea
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-12 18:30:00 -0500

    chore: remove extra comments/ planning

--- odin_projects/bubble_sort/bubble-sort.rb
+++ odin_projects/bubble_sort/bubble-sort.rb
@@ -4,16 +4,6 @@
 # Build a method #bubble_sort that takes an array and returns a sorted array. It must use the 
 # bubble sort methodology (using #sort would be pretty pointless, wouldn’t it?).
 # --------------------------------------------------------------------------------------------
-# 
-# Planning/ Ideation:
-# Input is an array, Output is an array.
-# Using a specific mechanism to order each individual item
-# reassigning indexes or swapping items in an array is going to be key here
-# iteration and a check to see if its sorted is key as well.
-# Optimization is a possible reach point, but probably beyond the necessary scope to get what
-# I need to out of this project
-# 
-# --------------------------------------------------------------------------------------------
 
 my_array = [2,6,1,5,8,9,3,4,7]
 
