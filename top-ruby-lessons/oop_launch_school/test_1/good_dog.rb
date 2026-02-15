29605343ccd45e5545a5a48b4656deb7f51264b1
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-15 18:30:00 -0500

    chore: practice class and module syntax

--- top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
+++ top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
@@ -1,4 +1,23 @@
-class GoodDog
+module Speak
+  def speak(sound)
+    puts sound
+  end
 end
 
+class GoodDog
+  include Speak
+end
+
+class HumanBeing
+  include Speak
+end
+
+dave = HumanBeing.new
 sparky = GoodDog.new
+
+
+sparky.speak("woof muthafucka")
+dave.speak("howdy")
+puts GoodDog.ancestors
+puts HumanBeing.ancestors
+
