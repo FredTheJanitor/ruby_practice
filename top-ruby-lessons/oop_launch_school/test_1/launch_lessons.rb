0707883230dd3f1f419854d618c310dda7fb275b
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-15 18:30:00 -0500

    chore: add a module with a method

--- top-ruby-lessons/oop_launch_school/test_1/launch_lessons.rb
+++ top-ruby-lessons/oop_launch_school/test_1/launch_lessons.rb
@@ -1,5 +1,13 @@
+module DoMove
+  def do_move
+    puts "do that move cause you love to groove"
+  end
+end
+
 class MyClass
-  
+  include DoMove
 end
 
 my_object = MyClass.new
+
+my_object.do_move
