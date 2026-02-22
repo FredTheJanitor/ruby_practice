a3fc37e6b335ada5e8d74741bf2fa8eb082e183b
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-22 18:30:00 -0500

    chore: update code according to lesson

--- top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
+++ top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
@@ -1,18 +1,11 @@
 class GoodDog
-  def initialize(name)
-    @name = name
-  end
-
-  # def name
-  #   @name
-  # end
-
-  # def name=(name)
-  #   @name = name
-  #   "setters ignore me, fish want me ;)"
-  # end
-  # both of these are replaced by the following shorthand:
   attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = h
+  end  
 
   def change_info(n,h,w)
     @name = n
