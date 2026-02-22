326e034e533bc980e4e076e1d887a86385b85e4b
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-22 18:30:00 -0500

    chore: code along lil doggy code along

--- top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
+++ top-ruby-lessons/oop_launch_school/test_1/good_dog.rb
@@ -16,15 +16,21 @@ class GoodDog
   def speak
     "#{name} says Arf!"
   end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
 end
 
-sparky = GoodDog.new("Sparky")
+sparky = GoodDog.new("Sparky", "6.9 inches", "6 lbs")
 puts sparky.speak
 
-fido = GoodDog.new("Fido")
+fido = GoodDog.new("Fido", "1000 feet", "1,000,000 lbs")
 puts fido.speak
 
 puts sparky.name
 
 sparky.name = "Lucifer Almighty, the Destroyer of God"
 puts sparky.name
+
+puts sparky.info
