18031f3a254863205388cf5eb4e0dc6c2c9eca0b
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-02-27 18:30:00 -0500

    chore: make more rubyish and protect state

--- top-ruby-lessons/oop_launch_school/test_1/foo.rb
+++ top-ruby-lessons/oop_launch_school/test_1/foo.rb
@@ -1,44 +1,55 @@
 class MyCar
-  attr_accessor :year, :color, :model, :speed, :stall, :crash, :running
+  attr_accessor :year, :color, :model
+  attr_reader :speed, :stall, :crash, :running
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
-    @running = "off"
-    @stall = "no"
-    @crash = "no"
+    @running = false
+    @stall = false
+    @crash = false
+  end
+
+  def running?
+    @running
+  end
+  def stall?
+    @stall
+  end
+  def crash?
+    @crash
   end
 
   def start
-    if running == "on"
-      return self.stall = "yes"
+    if running?
+      return @stall = true
     end
-    self.running = "on"
+    @running = true
   end
 
   def turn_off
-    self.running = "no"
+    @running = false
     if speed > 0
-      self.crash = "yes"
+      @crash = true
     end
-    self.speed = 0
+    @speed = 0
   end
 
   def speed_up
-    if running == "off"
-      self.stall = "yes"
+    unless running
+      @stall = true
     end
-    if running == "on"
-      self.speed += 5
+    if running?
+      @speed += 5
     end
     
   end
 
   def brake
     if speed > 0
-      self.speed -= 5
+      @speed -= 5
     end
   end
 
