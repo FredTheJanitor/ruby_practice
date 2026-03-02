98e91db58f893900ef74144474ec2424fa445350
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-02 18:30:00 -0500

    chore: get existential with it ya dig

--- top-ruby-lessons/oop_launch_school/test_1/good_dog_2.rb
+++ top-ruby-lessons/oop_launch_school/test_1/good_dog_2.rb
@@ -16,4 +16,11 @@ class GoodDog
     "I am a string with name: #{self.name} height: #{self.height} and weight: #{self.weight}"
   end
 
+  # this one is deep
+  def what_is_self
+    self
+  end
+
 end
+  sparky = GoodDog.new("Sparkison", "1000 miles", "a ton of feathers")
+  p sparky.what_is_self
