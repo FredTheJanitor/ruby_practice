2230fd4295318d3f777d577a52549719893c4593
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-07 18:30:00 -0500

    chore: examine inherited methods vs current class

--- top-ruby-lessons/oop_launch_school/test_1/new_test.rb
+++ top-ruby-lessons/oop_launch_school/test_1/new_test.rb
@@ -5,12 +5,22 @@ class Animal
 end
 
 class GoodDog < Animal
+  attr_accessor :name
+
+  def initialize(n)
+    self.name = n
+  end
+
+  def speak
+    "I am #{self.name} and I'm saying 'hey there'"
+  end
+
 end
 
 class Cat < Animal
 end
 
-sparky = GoodDog.new
+sparky = GoodDog.new("Sparky")
 paws = Cat.new
 puts sparky.speak
 puts paws.speak
