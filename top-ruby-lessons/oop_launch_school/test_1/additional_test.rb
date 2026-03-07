3cb5be29052e1b59fb5a62eed0b940dfad98d4ee
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-07 18:30:00 -0500

    chore: add new test page

---
+++ top-ruby-lessons/oop_launch_school/test_1/additional_test.rb
@@ -0,0 +1,30 @@
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class GoodDog < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+end
+
+bruno = GoodDog.new("brown")
+p bruno.name
+
+class BadDog < Animal
+  attr_accessor :age
+
+  def initialize(age, name)
+    super()
+    self.age = age
+  end
+end
+
+bear = BadDog.new(2, "bear")
+p bear.name
+p bear.age
