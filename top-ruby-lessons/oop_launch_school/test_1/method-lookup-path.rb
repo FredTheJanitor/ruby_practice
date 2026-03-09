5c444104804fc83de5162c1c24a41ca6aa403dca
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-09 18:30:00 -0400

    chore: show a method path ancestry chain

---
+++ top-ruby-lessons/oop_launch_school/test_1/method-lookup-path.rb
@@ -0,0 +1,28 @@
+module Walkable
+  def walk
+    "I'm Luke, luke sky walker"
+  end
+end
+
+module Swimmable
+  def swim
+    "hello i am doin a good swim swim"
+  end
+end
+
+module Climable
+  def climb
+    "I'm climbin brotha"
+  end
+end
+
+class Animal
+  include Walkable
+
+  def speak
+    "greetings, an animal is what I am!"
+  end
+end
+
+puts "--Animal path fpr method look up (ancestors)--"
+puts Animal.ancestors
