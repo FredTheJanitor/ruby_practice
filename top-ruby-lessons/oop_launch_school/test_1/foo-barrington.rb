c1096990d77bfa7ec1382ce5f000ace1fae45f64
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-10 18:30:00 -0400

    chore: show dangerous over-write and instance_of?

--- top-ruby-lessons/oop_launch_school/test_1/foo-barrington.rb
+++ top-ruby-lessons/oop_launch_school/test_1/foo-barrington.rb
@@ -8,9 +8,20 @@ class Child
   def say_hi
     p "b**** im diffrent"
   end
+
+  def send
+    p "send from child, DISRUPTING THE UNIVERSE MWAHAHA"
+  end
 end
 
 child = Child.new
 child.say_hi
 
 p Parent.superclass
+
+lad = Child.new
+# lad.send :say_hi  # this doesnt work right now
+
+c = Child.new
+p c.instance_of? Child
+p c.instance_of? Parent
