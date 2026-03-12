62417468bb9d20e1d4dbcb57f4a73c6ad31edb3f
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-12 18:30:00 -0400

    chore: mess with local variable gotchas

--- top-ruby-lessons/oop-continued/classes-ruby-explained.rb
+++ top-ruby-lessons/oop-continued/classes-ruby-explained.rb
@@ -1,11 +1,37 @@
 class Viking
+  attr_accessor :health
+
+
+  attr_accessor :health
+  attr_reader :name, :strength, :beard_length 
+
   def initialize(name, strength, beard_length, health)
     @name = name
     @strength = strength
     @beard_length = beard_length
     @health = health
   end
+  def attacks(enemy)
+    puts "#{name} attacks #{enemy.name} and does #{strength} damage"
+    enemy.takes_damage(strength)
+    if enemy.health <= 0
+      puts "#{enemy.name} was slain by #{name}"
+      enemy.health = 0 
+    end
+  end
+  def takes_damage(damage_amount)
+    @health -= damage_amount
+      if @health <= 0
+        @health = 0
+        puts "#{name} died a warrior's death!"
+      end
+  end
+
+  
 end
 
 brun = Viking.new("Brun", 65, 13, 100)
 p brun
+grob = Viking.new("Grob", 32, 4, 43)
+p grob
+brun.attacks(grob)
