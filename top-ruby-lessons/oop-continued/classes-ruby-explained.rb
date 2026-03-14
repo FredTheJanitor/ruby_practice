deba0ce1e80b676050a909ea6307b7ea075e2ba8
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-14 18:30:00 -0400

    chore: introduce randomness with class methods

--- top-ruby-lessons/oop-continued/classes-ruby-explained.rb
+++ top-ruby-lessons/oop-continued/classes-ruby-explained.rb
@@ -2,14 +2,36 @@ class Viking
   attr_accessor :health
   attr_reader :name, :strength, :beard_length
 
-  @@starting_health = 100
+  # @@starting_health = 100
 
-  def initialize(name, strength, beard_length)
+  def initialize(name, health, beard_length, strength)
     @name = name
     @strength = strength
     @beard_length = beard_length
-    @health = @@starting_health
+    @health = health
   end
+
+  def self.create_warrior(name)
+    beard_length = rand * 30 + 6
+    health = [beard_length * 5, 120].min
+    strength = [beard_length/2, 10].min
+    Viking.new(name, health, beard_length, strength)
+  end
+
+  def self.random_name
+    ["Erik", "Lars", "Leif", "Grud", "Brok", "Steave", "Minchin", "Harl", "Grob", "Brun", "Florgish", "Deen"].sample
+  end
+
+  def self.silver_to_gold(silver_pieces)
+    silver_pieces / 10
+  end
+
+  class << self #im diffrent yea im diffrent
+    def gold_to_silver(gold_pieces)
+      gold_pieces * 10
+    end
+  end
+
   def attacks(enemy)
     puts "#{name} attacks #{enemy.name} and does #{strength} damage"
     enemy.takes_damage(strength)
@@ -33,10 +55,16 @@ class Viking
   
 end
 
-brun = Viking.new("Brun", 65, 13)
+brun = Viking.new("Brun", 65, 18, 13)
 p brun
-grob = Viking.new("Grob", 32, 4)
+grob = Viking.new("Grob", 32, 7, 4)
 p grob
 brun.attacks(grob)
 grob.sleep
 p grob
+
+sten = Viking.create_warrior("Sten")
+puts sten.inspect
+
+warrior1 = Viking.create_warrior(Viking.random_name)
+p warrior1
