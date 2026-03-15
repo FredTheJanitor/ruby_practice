a81710f2b7309dc5d5f22eaab060f167bdf7035e
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-15 18:30:00 -0400

    chore: test example

--- top-ruby-lessons/oop-continued/medium_object_relationships.rb
+++ top-ruby-lessons/oop-continued/medium_object_relationships.rb
@@ -6,4 +6,34 @@ class Cinema
     @location = location
     @movies = []
   end
+
+  def add_movie(movie)
+    @movies << movie
+    movie.cinema = self
+  end
 end
+
+class Movie
+  
+  attr_accessor :title, :showtime, :cinema
+
+  @@all = []
+
+  def initialize(title, showtime)
+    @title = title
+    @showtime = showtime
+    @@all << self
+  end
+
+
+end
+
+cobble_hill = Cinema.new("Cobble Hill Cinema", "Court St, Brooklyn")
+p cobble_hill
+spider_man = Movie.new("Spider-Man", "9:00")
+p spider_man
+cobble_hill.add_movie(spider_man)
+
+p spider_man
+p cobble_hill
+
