6d426f9b11d9f3956d32e66b293db70a86620a33
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-16 18:30:00 -0400

    chore: demonstrate rescue and create next file

--- top-ruby-lessons/oop-continued/bastard1.rb
+++ top-ruby-lessons/oop-continued/bastard1.rb
@@ -1,10 +1,14 @@
-a = 10
-b = "42"
-begin
-  a + b
-rescue
-  puts "cant add variables a (#{a.class}) and b (#{b.class})" 
-else
-  puts "a + b is #{a+b}"
+values = [42, "a", "t", 9, 5, 213213, 8.7, "karen", "Libya", "Mars", "12", 98, rand + rand, {:dog => "cat"}, 100, nil, 200.0000, Object, 680, 3.14, "Goat", 78, true].shuffle
+while values.length > 0
+  a = values.pop
+  b = values.pop
+
+  begin
+    a + b
+  rescue
+    puts "cant add variables a (#{a.class}) and b (#{b.class})" 
+  else
+    puts "a + b is #{a+b}"
+  end
 end
 
