81af59b6117e6022a5f795b7075cc0e06cb045a3
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-04-01 18:30:00 -0400

    chore: add description of side-quest

---
+++ top-ruby-lessons/oop-continued/bastard-sidequest-scraper.rb
@@ -0,0 +1,75 @@
+#learned things from sidequest
+# 1) require & headers:
+# require "open-uri"
+# require "nokogiri"
+# headers = { "User-Agent" => Mozilla/5.0 (Windows NT... etc)
+# }
+# 
+# require statements:
+# open uri lets you open webpages
+# nokogiri lets you work with html'
+# 
+# headers:
+# allow a script to appear human
+# 
+# 2: Sitemaps
+# finding a sitemap on the robots.txt page 
+# and then leveraging that data to target
+# the specific urls you want to look at
+# 
+# I needed special tools to unzip the gz file
+# and then you use Regex to select the links
+# 
+# 3: Memory bank:
+# if File.exist?(csv_filename)
+#   CSV.foreach(csv_filename, headers: true) do |row|
+#   scraped_urls << row["WebSite URL"]
+#   end
+# end
+# 
+# target batch = all_entries.reject { |url, _| scraped_urls.include?(url) }
+# ***
+# 
+# 4: scraping:
+# def scrape_business_details(html, url, headers)
+# doc = Nokogiri::HTML(html)
+# name = doc.at_css("h1.title")&.text$.strip || "Unknown Shop"
+# 
+# nokogiri: makes the html readable and a tree
+# 
+# at_css this code finds specific tags within html
+# 
+# .& format if a business doesnt have a title
+# we dont want it to crash, it basically says if 
+# you get nothing, just move on and defaults to ||
+# option
+# 
+# 5) Ninja Backdoor AJAX
+# certain items on a website may not load immediately
+# one solution is finding where they are loaded from
+# and going straight to the source.
+# 
+# ajax_url = "#{url}?department_id=&sort=default"
+# ajax_headers = headers.merge({"X-Requested-With" => "XMLHttpRequest"})
+# 
+# this asks the server to skip loading the visual and just
+# hand over the raw data, then you can regex it
+# 
+# 6) saving and sleeping
+# 
+# so it is important to randomize and delay requests so
+# that way 1) you arent banned and 2) so it doesnt disrupt the 
+# website.
+# 
+# CSV.open(csv_filename, "ab") do |csv|
+# ...
+# sleep_time = rand(3..8)
+# sleep(sleep_time)
+# rescue Interrupt
+# exit
+# 
+# "ab" - append binary: this opens the CSV and drops data at
+# the bottom
+# 
+# rescue interrupt:
+# this stops ruby from freaking out when u ctrl c
