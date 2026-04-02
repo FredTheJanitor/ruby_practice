9f0abee8ede5bba24715aa3527a93f5ea9a16f07
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-04-02 18:30:00 -0400

    feat: add rescue interrupt

--- top-ruby-lessons/oop-continued/bastard2.rb
+++ top-ruby-lessons/oop-continued/bastard2.rb
@@ -1,9 +1,14 @@
 while 1
-  puts "Enter a number>>"
-  if num = Kernel.gets.match(/\d+/)
-    num = num[0]
-    puts "#{num} + 1 = #{num.to_i + 1}"
-  else
-    puts "Erroneous input! Try again..."
+  begin
+    puts "Enter a number>>"
+    if num = Kernel.gets.match(/\d+/)
+      num = num[0]
+      puts "#{num} + 1 = #{num.to_i + 1}"
+    else
+      puts "Erroneous input! Try again..."
+    end
+    puts "Press ctrl-C when you get bored"
+  rescue Interrupt => e
+    puts "what is Signal.trap??"
   end
 end
