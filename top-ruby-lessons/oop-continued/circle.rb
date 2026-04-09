34791fe8aac1359ad068ce9518b0af60c536e666
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-04-09 18:30:00 -0400

    demonstrate isntance methods and class methods

---
+++ top-ruby-lessons/oop-continued/circle.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+class Circle
+  @@PI = 3.141592
+
+  def initialize
+    @radius = 0
+  end
+
+  def set_radius radius
+    @radius = radius
+  end
+
+  def area
+    @radius * @radius * @@PI
+  end
+
+end
+
+c = Circle.new
+c.set_radius 5
+puts c.area
