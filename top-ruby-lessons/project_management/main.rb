d9ca1dc58fd11da5d8ccb91ebab625466f97dff4
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-05-06 18:30:00 -0400

    chore: update main with csv

--- top-ruby-lessons/project_management/main.rb
+++ top-ruby-lessons/project_management/main.rb
@@ -3,4 +3,6 @@
 # Convention is that require_relative is used for your own code, 
 # while require is used for things outside of it, like gems that 
 # your app depend on.
-require_relative 'lub/sort'
+require 'csv'
+
+require_relative 'lib/sort'
